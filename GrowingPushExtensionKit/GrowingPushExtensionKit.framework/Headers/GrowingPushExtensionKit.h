cf8e0814373f61ab3f8fae90b1bd913aacff3945
lvyuqiang <lvyuqiang@growingio.com>
2020-05-28 15:36:41 +0800

    Release 1.3.3

--- GrowingPushExtensionKit/GrowingPushExtensionKit.framework/Headers/GrowingPushExtensionKit.h
+++ GrowingPushExtensionKit/GrowingPushExtensionKit.framework/Headers/GrowingPushExtensionKit.h
@@ -9,13 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @class UNNotificationRequest;
+@class UNNotificationAttachment;
 
 @interface GrowingPushExtensionKit : NSObject
 
 //  pushExtensionKit 的版本号
-+ (NSString*)pushExtensionKitVersion;
-//  事件上报接口
-+ (void)sendNotificationRequest:(UNNotificationRequest *)request withCompletionHandler:(void (^)(id response))completionHandler;
++ (NSString *_Nonnull)pushExtensionKitVersion;
+
+/// 事件上报和多媒体推送支持接口
+/// @param request 推送请求
+/// @param handler 处理消息的回执，回调方法中处理管理的富媒体文件和错误信息(事件上报的错误和附件下载错误)
+/// @note  关联的富媒体文件，需要在GrowingIO 运营平台设置相关附件资源，SDK内部会自动下载
++ (void)handleNotificationRequest:(nonnull UNNotificationRequest *)request
+                   withCompletion:(nullable void(^)( NSArray <UNNotificationAttachment *>* _Nullable attachments,  NSArray <NSError *>* _Nullable errors))handler;
 
 @end
 
--- GrowingTouch.podspec
+++ GrowingTouch.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "GrowingTouch"
-  s.version      = "1.3.2"
+  s.version      = "1.3.3"
   s.summary      = "GrowingSDK-iOS-GrowingTouch"
 
   s.description  = <<-DESC
